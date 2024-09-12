23879046848d91653a4b71ed63d824e464bee8e4
Serhii Stanislav <serhiistanislav@gmail.com>
2024-09-12 15:15:30 +0300

    ADD | initial layout

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,5 @@
+const Main = () => {
+  return <header>main</header>;
+};
+
+export default Main;
---
+++ src/components/container/Container.tsx
@@ -0,0 +1,15 @@
+import { ReactElement } from 'react';
+import s from './Container.module.scss';
+
+type ContainerProps = {
+  label: 'app';
+  children: ReactElement;
+};
+
+type ContainerType = (props: ContainerProps) => ReactElement;
+
+const Container: ContainerType = ({ label, children }) => {
+  return <div className={`${s.container} ${s[label]}`}>{children}</div>;
+};
+
+export default Container;
